f97c30725d54579209dd20979f010d23fa7ebc0d
nilesh463 <132371186+nilesh463@users.noreply.github.com>
2025-02-27 18:35:53 +0530

    Gst Reports

---
+++ src/main/java/com/trueinsurre/controller/IndexCntroller.java
@@ -0,0 +1,19 @@
+package com.trueinsurre.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ch.qos.logback.core.model.Model;
+
+//@RequestMapping("/index")
+@Controller
+public class IndexCntroller {
+	
+	@GetMapping("/")
+	public ModelAndView indexPage(Model model) {
+		return new ModelAndView("index") ;
+	}
+
+}
